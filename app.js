7c51fdfb4d58f665a50b6581a916c03d8961913f
Vinoth Kumar <93541743+vkapgit@users.noreply.github.com>
2022-02-16 21:13:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/mithuntechnologies', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Hi There...!...Welcome to  Mithun Technologies. Please Contact +91-9980923226 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h2><center>Hi There...!......Welcome to  Mithun Technologies. Please Contact +91-9980923226 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
     response.end();
     
   })
